ef4083be5411a2d24737ce1dd49c2dee7d51364c
hank <hankchiu.tw@gmail.com>
2021-05-07 16:02:01 +0800

    initial commit

---
+++ apps/instapic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reap-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'instapic';
+}
