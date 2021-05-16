c5d805476f74aad56f31b11651dd8b959e7d7777
hank <hankchiu.tw@gmail.com>
2021-05-17 06:58:43 +0800

    feat: enable angular material

--- apps/instapic/src/app/app.module.ts
+++ apps/instapic/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { entityProviders } from './entities';
 import { AppRoutingModule } from './routing';
 import { SharedModule } from './shared';
 
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
   providers: [...entityProviders],
   bootstrap: [AppComponent],
 })
